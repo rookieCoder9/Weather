0a63d1b38db5446949ee0e40af697cc9221fe0ac
Sahil Kumar <sahil_kumar.ug20@nsut.ac.in>
2023-03-08 16:01:18 +0530

    Complete Subtask 1.1

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
-
-import { ICommandPalette } from '@jupyterlab/apputils';
+import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+import { Widget } from '@lumino/widgets';
 
 /**
  * Initialization data for the Weather extension.
@@ -15,6 +15,30 @@ const plugin: JupyterFrontEndPlugin<void> = {
   activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
     console.log('Weather is activate bye ! :)');
     console.log('ICommandPalette:', palette);
+    const newWidget = () => {
+      // Create a blank content widget inside of a MainAreaWidget
+      const content = new Widget();
+      const widget = new MainAreaWidget({ content });
+      widget.id = 'Weather';
+      widget.title.label = 'Weather';
+      widget.title.closable = true;
+      return widget;
+    };
+    let widget = newWidget();
+    const command = 'Weather:open';
+    app.commands.addCommand(command, {
+      label: 'Weather Application',
+      execute: () => {
+        if (widget.isDisposed) {
+          widget = newWidget();
+        }
+        if (!widget.isAttached) {
+          app.shell.add(widget, 'main');
+        }
+        app.shell.activateById(widget.id);
+      }
+    });
+    palette.addItem({ command, category: 'Tutorial' });
   }
 };
 
